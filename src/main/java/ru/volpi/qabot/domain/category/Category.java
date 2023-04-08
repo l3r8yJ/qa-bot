d089b341e8d4a4e1dd8b360529d289fa549daf86
Ivan <clicker.heroes.acg@gmail.com>
2023-04-09 02:28:40 +0300

    Category entity

---
+++ src/main/java/ru/volpi/qabot/domain/category/Category.java
@@ -0,0 +1,28 @@
+package ru.volpi.qabot.domain.category;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(schema = "categories_storage", name = "categories")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_name")
+    String name;
+}
