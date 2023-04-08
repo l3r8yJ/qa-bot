fe0f468ff988ba29d89be39b4214ccb9c56a2c48
Ivan <clicker.heroes.acg@gmail.com>
2023-04-09 01:10:11 +0300

    initial commit

---
+++ src/test/java/ru/volpi/qabot/QaBotApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.volpi.qabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
