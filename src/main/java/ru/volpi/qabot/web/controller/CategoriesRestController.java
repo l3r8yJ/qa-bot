4f5bddb3216bcb45145b2b4bf074ca9e296e91ab
Ivan <clicker.heroes.acg@gmail.com>
2023-04-09 22:52:57 +0300

    .env

--- src/main/java/ru/volpi/qabot/web/controller/CategoriesRestController.java
+++ src/main/java/ru/volpi/qabot/web/controller/CategoriesRestController.java
@@ -4,11 +4,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.volpi.qabot.dto.CategoryDto;
 import ru.volpi.qabot.exception.CategoryException;
 import ru.volpi.qabot.exception.CategoryNotFoundException;
 import ru.volpi.qabot.service.CategoriesService;
+import ru.volpi.qabot.web.controller.message.ControllerMessages;
 
 import java.util.List;
 
@@ -22,16 +30,22 @@ public class CategoriesRestController {
 
     @GetMapping
     public final List<CategoryDto> getAll() {
+        CategoriesRestController
+            .log.debug(ControllerMessages.REQUESTED_ALL_IN_CONTROLLER);
         return this.service.findAll();
     }
 
     @GetMapping("/{name}")
     public final ResponseEntity<?> getCategoryByName(@PathVariable final String name) {
+        CategoriesRestController
+            .log.debug(ControllerMessages.REQUESTED_CATEGORY_IN_CONTROLLER, name);
         return this.handleCategoryByName(name);
     }
     
     @PutMapping
     public final ResponseEntity<?> createCategory(@RequestBody final CategoryDto registration) {
+        CategoriesRestController
+            .log.debug(ControllerMessages.REGISTRATION_IN_CONTROLLER, registration);
         return this.handleCreateCategory(registration);
     }
 
---
+++ src/main/java/ru/volpi/qabot/web/controller/message/ControllerMessages.java
@@ -0,0 +1,17 @@
+package ru.volpi.qabot.web.controller.message;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ControllerMessages {
+
+    public static final String REQUESTED_ALL_IN_CONTROLLER
+        = "#getAll() called in controller";
+
+    public static final String REQUESTED_CATEGORY_IN_CONTROLLER
+        = "Requested category in controller :{}";
+
+    public static final String REGISTRATION_IN_CONTROLLER
+        = "Requested registration in controller :{}";
+
+}
