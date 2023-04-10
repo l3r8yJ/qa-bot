b41241931b24f2f06b3b2fbaba21874f15b90b62
Ivan <clicker.heroes.acg@gmail.com>
2023-04-10 11:49:44 +0300

    serial

--- src/main/java/ru/volpi/qabot/exception/category/CategoryException.java
+++ src/main/java/ru/volpi/qabot/exception/category/CategoryException.java
@@ -3,8 +3,13 @@ package ru.volpi.qabot.exception.category;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
+
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class CategoryException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 7678905234637313848L;
+
     public CategoryException(final String message) {
         super(message);
     }
