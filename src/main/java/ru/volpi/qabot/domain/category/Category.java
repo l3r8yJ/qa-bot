220d5a31ab2641ce2a8f1866dc201d6d5cc6eba0
Ivan <clicker.heroes.acg@gmail.com>
2023-04-10 21:50:55 +0300

    stackoverflow exception

--- src/main/java/ru/volpi/qabot/domain/category/Category.java
+++ src/main/java/ru/volpi/qabot/domain/category/Category.java
@@ -1,23 +1,12 @@
 package ru.volpi.qabot.domain.category;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import ru.volpi.qabot.domain.question.Question;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Table(schema = "categories_storage", name = "categories")
@@ -26,6 +15,8 @@ import java.util.Objects;
 @Builder
 @Setter
 @Getter
+@EqualsAndHashCode
+@ToString
 public class Category implements Serializable {
 
     @Serial
@@ -38,27 +29,8 @@ public class Category implements Serializable {
     @Column(name = "category_name")
     private String name;
 
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
     @OneToMany(mappedBy = "category")
     private List<Question> questions;
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(this.id) + Objects.hashCode(this.name);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (null == obj) {
-            return false;
-        }
-        if (!(obj instanceof Category)) {
-            return false;
-        }
-        return this.id == ((Category) obj).id && this.name == ((Category) obj).name;
-   }
-
-    @Override
-    public String toString() {
-        return "Category { 'id' = %d, 'name' = %s".formatted(this.id, this.name);
-    }
 }
--- src/main/java/ru/volpi/qabot/domain/question/Question.java
+++ src/main/java/ru/volpi/qabot/domain/question/Question.java
@@ -1,23 +1,11 @@
 package ru.volpi.qabot.domain.question;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import ru.volpi.qabot.domain.category.Category;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.Objects;
 
 @Entity
 @Table(schema = "questions_storage", name = "questions")
@@ -26,6 +14,8 @@ import java.util.Objects;
 @Builder
 @Setter
 @Getter
+@EqualsAndHashCode
+@ToString
 public class Question implements Serializable {
 
     @Serial
@@ -41,29 +31,9 @@ public class Question implements Serializable {
     @Column(name = "question_answer")
     private String answer;
 
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
     @ManyToOne
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(this.id);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (null == obj) {
-            return false;
-        }
-        if (!(obj instanceof Category)) {
-            return false;
-        }
-        return this.id == ((Category) obj).getId();
-    }
-
-    @Override
-    public String toString() {
-        return "Question {'text' = '%s', answer = '%s', 'category' = '%s' }"
-            .formatted(this.text, this.answer, this.category.getName());
-    }
 }
